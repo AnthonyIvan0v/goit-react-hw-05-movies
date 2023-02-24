25d2b3c78d314d935990d99503e4682b051c6f74
AnthonyIvan0v <cat.sledgehammer@gmail.com>
2023-02-24 16:23:22 +0200

    видаляє error

--- src/modules/Movies/Movies.jsx
+++ src/modules/Movies/Movies.jsx
@@ -6,20 +6,17 @@ import { getTrending } from 'services/movies-api';
 const Movies = () => {
   const [items, setItems] = useState([]);
 
-  const [error, setError] = useState(null);
-
   useEffect(() => {
     const fetchMovies = async () => {
       try {
         const data = await getTrending();
         setItems(data);
       } catch (error) {
-        setError(error.message);
       } finally {
       }
     };
     fetchMovies();
-  }, [setItems, setError]);
+  }, [setItems]);
 
   return <MoviesList items={items} />;
 };
